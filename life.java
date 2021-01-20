739538b83f900661b4094316fc800e71fb3a9715
marleelackey <74326328+marleelackey@users.noreply.github.com>
2021-01-20 09:49:35 -0500

    Update life.java
    In this project, I coded over zoom with my classmate Taryn, so we handled most of the big problems together, but I focused heavily on the later sections of checking the grid to find what position you are in

--- life.java
+++ life.java
@@ -1,6 +1,8 @@
 /**
  * HW 4 Question 4
  * @author Marlee Lackey
+ * In this project, I coded over zoom with my classmate Taryn, so we handled most of the big problems together,
+ * but I focused heavily on the later sections of checking the grid to find what position you are in 
  *
  */
 
